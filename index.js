fc33da76dac2149e31185f810ae30931ecfafa4b
Anastasiya Mudraya <mudraya.od@gmail.com>
2019-06-19 12:31:10 +0300

    zeit16

--- index.js
+++ index.js
@@ -25,10 +25,13 @@ const token = process.env['tg_api_key'] || '755380132:AAH326o9uguBRBOC9qpGX_n5Tv
 const bot = new TelegramBot(token, { polling: true })
 const url = 'https://test.mudrayaod.now.sh'
 
+const server = http.createServer((req, res) => {
+    // res.writeHeader(200, { 'Content-Type': 'text/html' });
     bot.setWebHook(`${url}/bot${token}`)
 
     bot.on('message', function onMessage(msg) {
         bot.sendMessage(msg.chat.id, 'I am alive on Zeit Now!');
     });
+})
 
-
+server.listen()
