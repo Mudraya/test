33d1a41b75ab8727a6652ab99c96ae2b9e57f3dd
Anastasiya Mudraya <mudraya.od@gmail.com>
2019-06-19 10:43:55 +0300

    zeit4

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
+//
+// const http = require('http')
+// const TelegramBot = require('node-telegram-bot-api')
+//
+// // require('http').createServer().listen(3000)
+//
+// const token = process.env['tg_api_key'] || '755380132:AAH326o9uguBRBOC9qpGX_n5TvQug85W8Ys'
+// const bot = new TelegramBot(token, { polling: true })
+// const url = 'https://test.mudrayaod.now.sh'
+//
+// bot.setWebHook(`${url}/bot${token}`)
+//
+// const app = require('express')()
+// app.get('*', (req, res) => {
+//     res.send('Hello from Express.js!')
+//     bot.on('message', function onMessage(msg) {
+//         bot.sendMessage(msg.chat.id, 'I am alive on Zeit Now!');
+//     });
+// })
+// app.listen()
 
-const http = require('http')
-const TelegramBot = require('node-telegram-bot-api')
+const http = require('http');
+const moment = require('moment');
 
-// require('http').createServer().listen(3000)
+const server = http.createServer((req, res) => {
+    res.writeHeader(200, { 'Content-Type': 'text/html' });
+    const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+    res.end(currentTime);
+});
 
-const token = process.env['tg_api_key'] || '755380132:AAH326o9uguBRBOC9qpGX_n5TvQug85W8Ys'
-const bot = new TelegramBot(token, { polling: true })
-const url = 'https://test.mudrayaod.now.sh'
-
-bot.setWebHook(`${url}/bot${token}`)
-
-const app = require('express')()
-app.get('*', (req, res) => {
-    res.send('Hello from Express.js!')
-    bot.on('message', function onMessage(msg) {
-        bot.sendMessage(msg.chat.id, 'I am alive on Zeit Now!');
-    });
-})
-app.listen()
-
+server.listen();
