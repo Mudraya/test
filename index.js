e4270250d083105ddb055af6c4dedceca7f80d8b
Anastasiya Mudraya <mudraya.od@gmail.com>
2019-06-19 16:17:00 +0300

    zeit32

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-//
-// const http = require('http')
-// const TelegramBot = require('node-telegram-bot-api')
-//
-// // require('http').createServer().listen(3000)
-//
-// const token = process.env['tg_api_key'] || '755380132:AAH326o9uguBRBOC9qpGX_n5TvQug85W8Ys'
-// const bot = new TelegramBot(token, { polling: true })
-// const url = 'https://test.mudrayaod.now.sh'
-//
-// bot.setWebHook(`${url}/bot${token}`)
-//
-// const app = require('express')()
-// app.get('*', (req, res) => {
-//     res.send('Hello from Express.js!')
-//     bot.on('message', function onMessage(msg) {
-//         bot.sendMessage(msg.chat.id, 'I am alive on Zeit Now!');
-//     });
-// })
-// app.listen()
-
 const http = require('http');
 const request = require('request');
-
-// const TelegramBot = require('node-telegram-bot-api')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
 const token = process.env['tg_api_key'] || '755380132:AAH326o9uguBRBOC9qpGX_n5TvQug85W8Ys'
-// const bot = new TelegramBot(token, { polling: true })
-const url = 'https://test.mudrayaod.now.sh'
+const webHookUrl = 'https://test.mudrayaod.now.sh'
 
 const sendMessage = (chat_id, text, res) => {
     const sendMessageUrl = `https://api.telegram.org/bot${token}/sendMessage`;
@@ -48,24 +26,6 @@ const sendMessage = (chat_id, text, res) => {
     )
 };
 
-http.createServer(function (req, res) {
-    let data = '';
-
-    req.on('data', chunk => {
-        data += chunk;
-    });
-
-    req.on('end', () => {
-        const parsedUpdate = data != "" ? JSON.parse(data) : {};
-        if (typeof parsedUpdate.message !== 'undefined') {
-            const text = parsedUpdate.message.text;
-            const chat_id = parsedUpdate.message.chat.id;
-
-            sendMessage(chat_id, 'I am alive on zeit Now!', res);
-        }
-    });
-}).listen(3000);
-
 const setWebHook = () => {
     const setWebhookUrl = `https://api.telegram.org/bot${token}/setWebhook`;
 
@@ -73,7 +33,7 @@ const setWebHook = () => {
             url: setWebhookUrl,
             method: 'post',
             body: {
-                url: url
+                url: webHookUrl
             },
             json: true
         },
@@ -83,6 +43,63 @@ const setWebHook = () => {
 };
 
 setWebHook()
+
+http.createServer(function (req, res) {
+    let data = '';
+
+    req.on('data', chunk => {
+        data += chunk;
+    });
+
+    req.on('end', () => {
+        const parsedUpdate = data !== "" ? JSON.parse(data) : {};
+        if (typeof parsedUpdate.message !== 'undefined') {
+            const text = parsedUpdate.message.text;
+            const chat_id = parsedUpdate.message.chat.id;
+            if (text === '/start') {
+                const start = 'Добро пожаловать!\nЧтобы узнать как пользоваться ботом используй команду /help'
+                sendMessage(chat_id, start, res)
+            } else if (text === '/help') {
+                const help = 'Давай же вместе узнаем, что интересного ждет тебя сегодня)\n' +
+                    'Надеюсь, ты знаешь свой зодиакальный знак) В зависимости от этого введи одно из следующих слов:\n\n' +
+                    'aries - если ты Овен\n' +
+                    'taurus - если ты Телец\n' +
+                    'gemini - если ты Близнецы\n' +
+                    'cancer - если ты Рак\n' +
+                    'leo - если ты Лев\n' +
+                    'virgo - если ты Дева\n' +
+                    'libra - если ты Весы\n' +
+                    'scorpio - если ты Скорпион\n' +
+                    'sagittarius - если ты Стрелец\n' +
+                    'capricorn - если ты Козерог\n' +
+                    'aquarius - если ты Водолей\n' +
+                    'pisces - если ты Рыбы\n'
+                sendMessage(chat_id, help, res)
+            } else if (text === 'aries' || text === 'taurus' || text === 'gemini' || text === 'cancer' || text === 'leo' || text === 'virgo'
+                || text === 'libra' || text === 'scorpio' || text === 'sagittarius' || text === 'capricorn' || text === 'aquarius' || text === 'pisces') {
+
+                let optionsJsdom = {
+                    referrer: 'http://astroscope.ru/horoskop/ejednevniy_goroskop/' + text + '.html'
+                }
+                let request = http.get('http://astroscope.ru/horoskop/ejednevniy_goroskop/' + text + '.html', function (response) {
+                    if (response.statusCode === 200) {
+                        JSDOM.fromURL('http://astroscope.ru/horoskop/ejednevniy_goroskop/' + text + '.html', optionsJsdom).then(dom => {
+                                let horoscope = dom.window.document.querySelectorAll('.p-3')[1].innerHTML
+                                sendMessage(chat_id, horoscope, res)
+                            }
+                        )
+                    }
+                })
+                request.on('error', function (error) {
+                    console.error(error.status)
+                })
+
+            } else { sendMessage(chat_id, 'Пожалуйста, придерживайся инструкции ;)', res) }
+        }
+    });
+}).listen(3000);
+
+
 
 
 
